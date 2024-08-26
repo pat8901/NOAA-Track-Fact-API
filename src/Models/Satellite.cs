f2c07213b5426c5dd4c2c805e883f6f9ee7d24aa
Patrick <seitzpr@gmail.com>
2024-08-25 23:03:52 -0500

    Added database context and model

---
+++ src/Models/Satellite.cs
@@ -0,0 +1,9 @@
+namespace NOAA_API.Models
+{
+    public class Satellite
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime LaunchDate { get; set; }
+    }
+}
